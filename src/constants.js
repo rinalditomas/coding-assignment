1404d0c1e94971071809500fd000c4a71379de55
Tomas Rinaldi Scatena <76567220+rinalditomas@users.noreply.github.com>
2024-05-12 10:49:57 +0200

    remove unused constant

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,3 @@ export const API_KEY = process.env.API_KEY
 export const ENDPOINT = process.env.ENDPOINT
 export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie/?api_key='+API_KEY+'&sort_by=vote_count.desc'
 export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie/?api_key='+API_KEY
-export const ENDPOINT_MOVIE = ENDPOINT+'/movie/507086?api_key='+API_KEY+'&append_to_response=videos'
